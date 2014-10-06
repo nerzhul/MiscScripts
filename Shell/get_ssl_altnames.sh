5b7d3705d92f76d66b055598173b704b1a0ac457
Ner'zhul <nerzhul@users.noreply.github.com>
2014-10-06 11:25:54 +0200

    Forget to exit on error :(

--- Shell/get_ssl_altnames.sh
+++ Shell/get_ssl_altnames.sh
@@ -3,6 +3,7 @@
 if [ -z $1 ]
 then
   echo "No argument given. Waiting a DNS name or an IP"
+  exit 1;
 fi
 
 for N in $(openssl s_client -connect $1:443 </dev/null 2>&1 | openssl x509 -text|grep -A1 "X509v3 Subject Alternative Name"|tail -1|sed 's/[DNS:|,]//g')
