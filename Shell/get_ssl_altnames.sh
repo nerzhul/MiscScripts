2a95c09fee79efd221c07fbf8e31c39d216f9ba5
Ner'zhul <nerzhul@users.noreply.github.com>
2014-10-06 11:05:31 +0200

    Create get_ssl_altnames.sh

---
+++ Shell/get_ssl_altnames.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+if [ -z $1 ]
+then
+  echo "No argument given. Waiting a DNS name or an IP"
+fi
+
+for N in $(openssl s_client -connect $1:443 </dev/null 2>&1 | openssl x509 -text|grep -A1 "X509v3 Subject Alternative Name"|tail -1|sed 's/[DNS:|,]//g')
+do
+  echo "=== $N ==="
+  dig +short $N
+done
